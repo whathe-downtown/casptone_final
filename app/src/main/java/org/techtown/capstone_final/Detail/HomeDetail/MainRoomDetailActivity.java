113ec939f21abdf6a553e363953c440a7ccef4ff
hg960 <hg971027@gmail.com>
2021-11-20 18:12:54 +0900

    2021-11-16 dong

--- app/src/main/java/org/techtown/capstone_final/Detail/HomeDetail/MainRoomDetailActivity.java
+++ app/src/main/java/org/techtown/capstone_final/Detail/HomeDetail/MainRoomDetailActivity.java
@@ -30,10 +30,11 @@ public class MainRoomDetailActivity extends AppCompatActivity {
     Button spread_date,spread_time,spread_place,spread_Personnel,spread_link;
     Button submit_date,submit_time,submit_place,submit_Personnel,submit_link;
     LinearLayout Layout_Get_Date,Layout_Get_Time,Layout_Get_place,Layout_Get_Personnel,Layout_Get_Link;
-    TextView view_date,view_time,view_palce,view_personnel,view_link;
+    TextView view_date,view_time,view_place,view_personnel,view_link;
     TimePicker timePicker;
     DatePicker datePicker;
-    Calendar C;
+    Calendar c;
+    TextView getplace,getpersonnel,getlink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +51,7 @@ public class MainRoomDetailActivity extends AppCompatActivity {
 
         spread_date = findViewById(R.id.spread_date);
         spread_time = findViewById(R.id.spread_time);
-        spread_place = findViewById(R.id.spread_palce);
+        spread_place = findViewById(R.id.spread_place);
         spread_Personnel = findViewById(R.id.spread_Personnel);
         spread_link = findViewById(R.id.spread_Link);
 
@@ -68,18 +69,23 @@ public class MainRoomDetailActivity extends AppCompatActivity {
 
         view_date = findViewById(R.id.view_date);
         view_time = findViewById(R.id.view_time);
-        view_palce = findViewById(R.id.view_palce);
+        view_place = findViewById(R.id.view_palce);
         view_personnel = findViewById(R.id.view_personnel);
         view_link = findViewById(R.id.view_link);
 
         datePicker = findViewById(R.id.date_picker);
         timePicker = findViewById(R.id.time_picker);
+        getplace = findViewById(R.id.Get_place);
+        getpersonnel = findViewById(R.id.Get_Personnel);
+        getlink = findViewById(R.id.Get_Link);
+
 
         view_date.setText("언제 모이나요?");
         view_time.setText("몇시에 모이나요?");
-        view_palce.setText("어디소 모이나요?");
+        view_place.setText("어디소 모이나요?");
         view_personnel.setText("몇명이서 모이나요?");
         view_link.setText("채팅방 주소를 알려주세요");
+
 
 
         /* 데이터 피커 */
@@ -106,6 +112,91 @@ public class MainRoomDetailActivity extends AppCompatActivity {
 
         /* 타임피커 피커 */
 
+        spread_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Layout_Get_Time.setVisibility(Layout_Get_Time.VISIBLE);
+                spread_time.setRotationX(180);
+            }
+        });
+
+        submit_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int hourOfDay = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+                view_time.setText(hourOfDay+"시"+minute+"분");
+/*
+                timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+                    @Override
+                    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                        view_time.setText(hourOfDay+"시"+minute+"분");
+                    }
+                });
+*/
+                Layout_Get_Time.setVisibility(Layout_Get_Time.GONE);
+                spread_time.setRotationX(0);
+            }
+        });
+
+        /* 장소 선택 */
+
+        spread_place.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Layout_Get_place.setVisibility(Layout_Get_place.VISIBLE);
+                spread_place.setRotationX(180);
+            }
+        });
+
+        submit_place.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Layout_Get_place.setVisibility(Layout_Get_place.GONE);
+                spread_place.setRotationX(0);
+                view_place.setText(getplace.getText());
+            }
+        });
+
+        /* 인원 선택 */
+
+        spread_Personnel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Layout_Get_Personnel.setVisibility(Layout_Get_Personnel.VISIBLE);
+                spread_Personnel.setRotationX(180);
+            }
+        });
+
+        submit_Personnel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Layout_Get_Personnel.setVisibility(Layout_Get_Personnel.GONE);
+                spread_Personnel.setRotationX(0);
+                view_personnel.setText(getpersonnel.getText());
+            }
+        });
+
+        /* 채팅방 링크 */
+
+        spread_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Layout_Get_Link.setVisibility(Layout_Get_Link.VISIBLE);
+                spread_link.setRotationX(180);
+            }
+        });
+        submit_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Layout_Get_Link.setVisibility(Layout_Get_Link.GONE);
+                spread_link.setRotationX(0);
+            }
+        });
 
 
     }
