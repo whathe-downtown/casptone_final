9ad9b964c6eac66ff780d52936c1ff08f5a834b3
hg960 <hg971027@gmail.com>
2021-11-26 02:15:53 +0900

    2021-11-16 dong

--- app/src/main/java/org/techtown/capstone_final/MakeRoomContianerActivity.java
+++ app/src/main/java/org/techtown/capstone_final/MakeRoomContianerActivity.java
@@ -1,7 +1,5 @@
 package org.techtown.capstone_final;
 
-import static android.service.controls.ControlsProviderService.TAG;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,6 +20,8 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import org.techtown.capstone_final.databinding.ActivityMakeContainerBinding;
+
+import static android.service.controls.ControlsProviderService.TAG;
 public class MakeRoomContianerActivity extends AppCompatActivity {
     ActivityMakeContainerBinding binding;
     FirebaseAuth auth;
@@ -180,197 +180,197 @@ public class MakeRoomContianerActivity extends AppCompatActivity {
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.Roomprofilepic:
-                        Roomprofileupdate();
-                        break;
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.Roomprofilepic:
+                    Roomprofileupdate();
+                    break;
 
-                    case R.id.makeroom_next2:
-                        save();
-                        break;
+                case R.id.makeroom_next2:
+                    save();
+                    break;
 
-                    case R.id.spread_date:
-                        if(state[0]==0){
-                            binding.makeRoom2.LayoutGetDate.setVisibility(View.VISIBLE);
-                            binding.makeRoom2.spreadDate.setRotation(180);
-                            state[0]++;}
-                        else{
-                            binding.makeRoom2.LayoutGetDate.setVisibility(View.GONE);
-                            binding.makeRoom2.spreadDate.setRotation(0);
-                            state[0]--;}
-                        break;
-                    case R.id.spread_time:
-                        if(state[1]==0){
-                            binding.makeRoom2.LayoutGetTime.setVisibility(View.VISIBLE);
-                            binding.makeRoom2.spreadTime.setRotation(180);
-                            state[1]++;}
-                        else{
-                            binding.makeRoom2.LayoutGetTime.setVisibility(View.GONE);
-                            binding.makeRoom2.spreadTime.setRotation(0);
-                            state[1]--;}
-                        break;
-                    case R.id.spread_place:
-                        if(state[2]==0){
-                            binding.makeRoom2.LayoutGetPlace.setVisibility(View.VISIBLE);
-                            binding.makeRoom2.spreadPlace.setRotation(180);
-                            state[2]++;}
-                        else{
-                            binding.makeRoom2.LayoutGetPlace.setVisibility(View.GONE);
-                            binding.makeRoom2.spreadPlace.setRotation(0);
-                            state[2]--;}
-                        break;
-                    case R.id.spread_Personnel:
-                        if(state[3]==0){
-                            binding.makeRoom2.LayoutGetPersonnel.setVisibility(View.VISIBLE);
-                            binding.makeRoom2.spreadPersonnel.setRotation(180);
-                            state[3]++;}
-                        else{
-                            binding.makeRoom2.LayoutGetPersonnel.setVisibility(View.GONE);
-                            binding.makeRoom2.spreadPersonnel.setRotation(0);
-                            state[3]--;}
-                        break;
-                    case R.id.spread_Link:
-                        if(state[4]==0){
-                            binding.makeRoom2.LayoutGetLink.setVisibility(View.VISIBLE);
-                            binding.makeRoom2.spreadLink.setRotation(180);
-                            state[4]++;}
-                        else{
-                            binding.makeRoom2.LayoutGetLink.setVisibility(View.GONE);
-                            binding.makeRoom2.spreadLink.setRotation(0);
-                            state[4]--;}
-                        break;
+                case R.id.spread_date:
+                    if(state[0]==0){
+                        binding.makeRoom2.LayoutGetDate.setVisibility(View.VISIBLE);
+                        binding.makeRoom2.spreadDate.setRotation(180);
+                        state[0]++;}
+                    else{
+                        binding.makeRoom2.LayoutGetDate.setVisibility(View.GONE);
+                        binding.makeRoom2.spreadDate.setRotation(0);
+                        state[0]--;}
+                    break;
+                case R.id.spread_time:
+                    if(state[1]==0){
+                        binding.makeRoom2.LayoutGetTime.setVisibility(View.VISIBLE);
+                        binding.makeRoom2.spreadTime.setRotation(180);
+                        state[1]++;}
+                    else{
+                        binding.makeRoom2.LayoutGetTime.setVisibility(View.GONE);
+                        binding.makeRoom2.spreadTime.setRotation(0);
+                        state[1]--;}
+                    break;
+                case R.id.spread_place:
+                    if(state[2]==0){
+                        binding.makeRoom2.LayoutGetPlace.setVisibility(View.VISIBLE);
+                        binding.makeRoom2.spreadPlace.setRotation(180);
+                        state[2]++;}
+                    else{
+                        binding.makeRoom2.LayoutGetPlace.setVisibility(View.GONE);
+                        binding.makeRoom2.spreadPlace.setRotation(0);
+                        state[2]--;}
+                    break;
+                case R.id.spread_Personnel:
+                    if(state[3]==0){
+                        binding.makeRoom2.LayoutGetPersonnel.setVisibility(View.VISIBLE);
+                        binding.makeRoom2.spreadPersonnel.setRotation(180);
+                        state[3]++;}
+                    else{
+                        binding.makeRoom2.LayoutGetPersonnel.setVisibility(View.GONE);
+                        binding.makeRoom2.spreadPersonnel.setRotation(0);
+                        state[3]--;}
+                    break;
+                case R.id.spread_Link:
+                    if(state[4]==0){
+                        binding.makeRoom2.LayoutGetLink.setVisibility(View.VISIBLE);
+                        binding.makeRoom2.spreadLink.setRotation(180);
+                        state[4]++;}
+                    else{
+                        binding.makeRoom2.LayoutGetLink.setVisibility(View.GONE);
+                        binding.makeRoom2.spreadLink.setRotation(0);
+                        state[4]--;}
+                    break;
 
-                    case R.id.submit_date:
-                        int Year = binding.makeRoom2.datePicker.getYear();
-                        int Month = binding.makeRoom2.datePicker.getMonth();
-                        int DayOfMonth = binding.makeRoom2.datePicker.getDayOfMonth();
-                        binding.makeRoom2.viewDate.setText(Year + "년 " + Month + "월 " + DayOfMonth + "일" );
+                case R.id.submit_date:
+                    int Year = binding.makeRoom2.datePicker.getYear();
+                    int Month = binding.makeRoom2.datePicker.getMonth();
+                    int DayOfMonth = binding.makeRoom2.datePicker.getDayOfMonth();
+                    binding.makeRoom2.viewDate.setText(Year + "년 " + Month + "월 " + DayOfMonth + "일" );
 
-                        break;
-                    case R.id.submit_time:
-                        int hourOfDay = binding.makeRoom2.timePicker.getCurrentHour();
-                        int minute = binding.makeRoom2.timePicker.getCurrentMinute();
-                        binding.makeRoom2.viewTime.setText(hourOfDay+"시 "+minute+"분");
+                    break;
+                case R.id.submit_time:
+                    int hourOfDay = binding.makeRoom2.timePicker.getCurrentHour();
+                    int minute = binding.makeRoom2.timePicker.getCurrentMinute();
+                    binding.makeRoom2.viewTime.setText(hourOfDay+"시 "+minute+"분");
 
-                        break;
-                    case R.id.submit_place:
-                        
-                        break;
-                    case R.id.submit_personnel:
+                    break;
+                case R.id.submit_place:
 
-                        break;
-                    case R.id.submit_link:
+                    break;
+                case R.id.submit_personnel:
 
-                        break;
-                }
+                    break;
+                case R.id.submit_link:
+
+                    break;
             }
-        };
-
-
-
-/*
-        //날짜 선택
-        binding.makeRoom2.spreadDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.makeRoom2.LayoutGetDate.setVisibility(binding.makeRoom2.LayoutGetDate.VISIBLE);
-                binding.makeRoom2.spreadDate.setRotationX(180);
-            }
-        });
-
-
-
-        binding.makeRoom2.submitTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int hourOfDay = binding.makeRoom2.timePicker.getCurrentHour();
-                int minute = binding.makeRoom2.timePicker.getCurrentMinute();
-                binding.makeRoom2.viewTime.setText(hourOfDay+"시"+minute+"분");
-
-
-                timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-                    @Override
-                    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                        view_time.setText(hourOfDay+"시"+minute+"분");
-                    }
-                });
-
-                binding.makeRoom2.LayoutGetTime.setVisibility(binding.makeRoom2.LayoutGetTime.GONE);
-                binding.makeRoom2.spreadTime.setRotationX(0);
-            }
-        });
-        //장소 선택
-
-        binding.makeRoom2.spreadPlace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.makeRoom2.LayoutGetPlace.setVisibility(binding.makeRoom2.LayoutGetPlace.VISIBLE);
-                binding.makeRoom2.spreadPlace.setRotationX(180);
-            }
-        });
-
-        binding.makeRoom2.submitPlace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                binding.makeRoom2.LayoutGetPlace.setVisibility(binding.makeRoom2.LayoutGetPlace.GONE);
-                binding.makeRoom2.spreadPlace.setRotationX(0);
-                binding.makeRoom2.viewPalce.setText(binding.makeRoom2.GetPersonnel.getText());
-            }
-        });
-
-        // 인원 선택
-
-        binding.makeRoom2.spreadPersonnel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.makeRoom2.LayoutGetPersonnel.setVisibility( binding.makeRoom2.LayoutGetPersonnel.VISIBLE);
-                binding.makeRoom2.spreadPersonnel.setRotationX(180);
-            }
-        });
-
-        binding.makeRoom2.submitPersonnel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                binding.makeRoom2.LayoutGetPersonnel.setVisibility(binding.makeRoom2.LayoutGetPersonnel.GONE);
-                binding.makeRoom2.spreadPersonnel.setRotationX(0);
-                binding.makeRoom2.viewPersonnel.setText(binding.makeRoom2.GetPersonnel.getText());
-            }
-        });
-
-        //채팅방 링크
-
-        binding.makeRoom2.spreadLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.makeRoom2.LayoutGetLink.setVisibility(  binding.makeRoom2.LayoutGetLink.VISIBLE);
-                binding.makeRoom2.spreadLink.setRotationX(180);
-            }
-        });
-        binding.makeRoom2.submitLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                binding.makeRoom2.LayoutGetLink.setVisibility(binding.makeRoom2.LayoutGetLink.GONE);
-                binding.makeRoom2.spreadLink.setRotationX(0);
-            }
-        });
-
-    }
-    public void onBackPressed() {
-        super.onBackPressed();
-        if (page ==2){
-            binding.makeRoom1.getRoot().setVisibility(View.VISIBLE);
-            binding.makeRoom2.getRoot().setVisibility(View.GONE);
-            page--;
-        }else{
-            Intent intent = new Intent(MakeRoomContianerActivity.this, HomeActivity.class);
-            startActivity(intent);
         }
-*/
+    };
+
+
+
+    /*
+            //날짜 선택
+            binding.makeRoom2.spreadDate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    binding.makeRoom2.LayoutGetDate.setVisibility(binding.makeRoom2.LayoutGetDate.VISIBLE);
+                    binding.makeRoom2.spreadDate.setRotationX(180);
+                }
+            });
+
+
+
+            binding.makeRoom2.submitTime.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    int hourOfDay = binding.makeRoom2.timePicker.getCurrentHour();
+                    int minute = binding.makeRoom2.timePicker.getCurrentMinute();
+                    binding.makeRoom2.viewTime.setText(hourOfDay+"시"+minute+"분");
+
+
+                    timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+                        @Override
+                        public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                            view_time.setText(hourOfDay+"시"+minute+"분");
+                        }
+                    });
+
+                    binding.makeRoom2.LayoutGetTime.setVisibility(binding.makeRoom2.LayoutGetTime.GONE);
+                    binding.makeRoom2.spreadTime.setRotationX(0);
+                }
+            });
+            //장소 선택
+
+            binding.makeRoom2.spreadPlace.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    binding.makeRoom2.LayoutGetPlace.setVisibility(binding.makeRoom2.LayoutGetPlace.VISIBLE);
+                    binding.makeRoom2.spreadPlace.setRotationX(180);
+                }
+            });
+
+            binding.makeRoom2.submitPlace.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    binding.makeRoom2.LayoutGetPlace.setVisibility(binding.makeRoom2.LayoutGetPlace.GONE);
+                    binding.makeRoom2.spreadPlace.setRotationX(0);
+                    binding.makeRoom2.viewPalce.setText(binding.makeRoom2.GetPersonnel.getText());
+                }
+            });
+
+            // 인원 선택
+
+            binding.makeRoom2.spreadPersonnel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    binding.makeRoom2.LayoutGetPersonnel.setVisibility( binding.makeRoom2.LayoutGetPersonnel.VISIBLE);
+                    binding.makeRoom2.spreadPersonnel.setRotationX(180);
+                }
+            });
+
+            binding.makeRoom2.submitPersonnel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    binding.makeRoom2.LayoutGetPersonnel.setVisibility(binding.makeRoom2.LayoutGetPersonnel.GONE);
+                    binding.makeRoom2.spreadPersonnel.setRotationX(0);
+                    binding.makeRoom2.viewPersonnel.setText(binding.makeRoom2.GetPersonnel.getText());
+                }
+            });
+
+            //채팅방 링크
+
+            binding.makeRoom2.spreadLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    binding.makeRoom2.LayoutGetLink.setVisibility(  binding.makeRoom2.LayoutGetLink.VISIBLE);
+                    binding.makeRoom2.spreadLink.setRotationX(180);
+                }
+            });
+            binding.makeRoom2.submitLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    binding.makeRoom2.LayoutGetLink.setVisibility(binding.makeRoom2.LayoutGetLink.GONE);
+                    binding.makeRoom2.spreadLink.setRotationX(0);
+                }
+            });
+
+        }
+        public void onBackPressed() {
+            super.onBackPressed();
+            if (page ==2){
+                binding.makeRoom1.getRoot().setVisibility(View.VISIBLE);
+                binding.makeRoom2.getRoot().setVisibility(View.GONE);
+                page--;
+            }else{
+                Intent intent = new Intent(MakeRoomContianerActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+    */
     private void startToast(String msg) { Toast.makeText(this, msg, Toast.LENGTH_SHORT).show(); }
 }
