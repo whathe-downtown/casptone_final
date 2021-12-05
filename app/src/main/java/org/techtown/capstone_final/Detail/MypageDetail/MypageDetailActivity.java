fb1e6c00d72bca70c18aaa6e119528afaeda3799
Chungkyu <dbcjdrb1997@gmail.com>
2021-12-05 17:06:46 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    2021 - 11-24 messasge
    2021 - 11-25 messasge
    2021 - 11-25 messasge -01
    2021 - 12-03 messasge -01
    2021 - 12-04 messasge -01

--- app/src/main/java/org/techtown/capstone_final/Detail/MypageDetail/MypageDetailActivity.java
+++ app/src/main/java/org/techtown/capstone_final/Detail/MypageDetail/MypageDetailActivity.java
@@ -158,6 +158,7 @@ public class MypageDetailActivity extends AppCompatActivity {
 
                 case R.id.profile_update:
                     userinfo();
+                    chipsave();
                     Intent intent = new Intent(MypageDetailActivity.this,MypageActivity.class);
                     intent.setFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
                     finish();
@@ -215,6 +216,83 @@ public class MypageDetailActivity extends AppCompatActivity {
 //                }
 //            }
 //        });
+    }
+    public void chipsave()
+    {
+        // db 안에 document 삭제 (모든데이터 날아감)
+
+        for(int i=0;i<14;i++){
+            if(ChipArray[i]==1){
+                switch (i){
+                    case 1 :
+                        //인문을 파이어 베이스에 저장
+                        db.collection("users").document(FirebaseAuth.getInstance().getUid()).collection("category").document("category")
+                                .update("인문", 15)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        startToast("인문 값이 저장되었습니다.");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            startToast("인문 값이 저장안되었습니다.");
+                            Log.w(TAG, "Error updating document", e);
+                        }
+                    });
+                        startToast("인문");
+                        break;
+                    case 2 :
+                        //미술을 파이어 베이스에 저장
+                        startToast("미술");
+                        break;
+                    case 3 :
+                        //법과을 파이어 베이스에 저장
+                        startToast("법과");
+                        break;
+                    case 4 :
+                        //경영을 파이어 베이스에 저장
+                        startToast("경영");
+                        break;
+                    case 5 :
+                        //음악을 파이어 베이스에 저장
+                        startToast("법과");
+                        break;
+                    case 6 :
+                        //공과을 파이어 베이스에 저장
+                        startToast("공과");
+                        break;
+                    case 7 :
+                        //정보을 파이어 베이스에 저장
+                        startToast("정보");
+                        break;
+                    case 8 :
+                        //농과을 파이어 베이스에 저장
+                        startToast("농과");
+                        break;
+                    case 9 :
+                        //체육을 파이어 베이스에 저장
+                        startToast("체육");
+                        break;
+                    case 10 :
+                        //수산을 파이어 베이스에 저장
+                        break;
+                    case 11 :
+                        //예술을 파이어 베이스에 저장
+                        break;
+                    case 12 :
+                        //사회과학을 파이어 베이스에 저장
+                        break;
+                    case 13 :
+                        //자연과학을 파이어 베이스에 저장
+                        break;
+                    case 14 :
+                        //생활과학을 파이어 베이스에 저장
+                        break;
+                }
+            }
+        }
     }
     private void back(){
         Intent intent = new Intent(MypageDetailActivity.this, MypageActivity.class);
