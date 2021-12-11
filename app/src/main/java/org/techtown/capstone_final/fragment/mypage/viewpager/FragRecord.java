46e63b17b60040f237fd7ddd7caaa321fa73e894
Chungkyu <dbcjdrb1997@gmail.com>
2021-12-11 21:49:52 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    2021 - 11-24 messasge
    2021 - 11-25 messasge
    2021 - 11-25 messasge -01
    2021 - 12-03 messasge -01
    2021 - 12-04 messasge -01

--- app/src/main/java/org/techtown/capstone_final/fragment/mypage/viewpager/FragRecord.java
+++ app/src/main/java/org/techtown/capstone_final/fragment/mypage/viewpager/FragRecord.java
@@ -1,6 +1,7 @@
 package org.techtown.capstone_final.fragment.mypage.viewpager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +11,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.techtown.capstone_final.Adapters.RecordAdapter;
 import org.techtown.capstone_final.Model.Record;
@@ -44,11 +50,29 @@ public class FragRecord extends Fragment {
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         binding.recordRecyclerview.setLayoutManager(layoutManager);
+
+        CollectionReference coRef = db.collection("record");
+
+        coRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Record record= document.toObject(Record.class);
+                        list.add(record);
+
+                    }
+                }else{
+                    Log.d("FragRecord", "Current data: null");
+
+
+                }adapter.notifyDataSetChanged();
+            }
+        });
+
+
+
         return view;
     }
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_frag_record);
-//    }
+
 }
